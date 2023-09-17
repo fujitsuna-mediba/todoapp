e2827447235e2f39871cd58bfe040b0c8a71172b
fujitsuna-mediba <fujitsuna@mediba.jp>
2023-09-17 23:11:33 +0900

    delete Test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
-import Test from './Test'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Test />
+    <App />
     <Analytics />
   </React.StrictMode>
 )
