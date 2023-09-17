1b7219169b42d0730300edf4325389b01b3dbe47
fujitsuna-mediba <fujitsuna@mediba.jp>
2023-09-17 21:16:21 +0900

    test kuma-ui

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import logo from './assets/logo.png'
-
+import { Box, Text } from '@kuma-ui/core'
 const Test: React.FC = () => {
   const handleOnClick = () => {
     console.log('clicked')
   }
   return (
-    <div>
-      <h1>Test</h1>
+    <Box>
+      <Text as='h1'>Test</Text>
       <img src={logo} alt='logo' width='188' height='60' onClick={handleOnClick} />
-    </div>
+    </Box>
   )
 }
 
